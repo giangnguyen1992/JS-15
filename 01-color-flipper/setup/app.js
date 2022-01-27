a51bad254c69046bead6f7470f85d696034c464f
giangnguyen1992 <tgn1992@hotmail.de>
2022-01-27 19:29:29 +0100

    finished first project

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,20 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.querySelector("#btn");
+const colorBox = document.querySelector(".color");
+
+function generateColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function printColor(color) {
+  colorBox.innerText = color;
+}
+
+function setBgColor() {
+  const color = generateColor();
+  printColor(color);
+  document.documentElement.style.setProperty("--bg-color", color);
+}
+
+btn.addEventListener("click", setBgColor);
+setBgColor();
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,23 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector("#btn");
+const colorBox = document.querySelector(".color");
+
+function generateColor() {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[Math.floor(Math.random() * hex.length)].toString();
+  }
+  return hexColor;
+}
+
+function printColor(color) {
+  colorBox.innerText = color;
+}
+
+function setBgColor() {
+  const color = generateColor();
+  printColor(color);
+  document.documentElement.style.setProperty("--bg-color", color);
+}
+
+btn.addEventListener("click", setBgColor);
