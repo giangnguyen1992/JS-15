c504d7b33495d51cdd876dadcd3ef8d9e86ea69c
giangnguyen1992 <tgn1992@hotmail.de>
2022-01-28 21:37:41 +0100

    added more prject

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -11,3 +11,61 @@
 
 // ********** smooth scroll ************
 // select links
+
+const target = document.querySelector("#about .title");
+const nav = document.querySelector("#nav");
+const scrollButton = document.querySelector(".btn-white");
+const toursSection = document.querySelector("#tours");
+
+const options = {
+  rootMargin: "0px",
+  threshold: [0, 0.25, 0.5, 0.75, 1],
+};
+
+let previousY = 0;
+let previousRatio = 0;
+
+const handleIntersect = (entries) => {
+  entries.forEach((entry) => {
+    const currentY = entry.boundingClientRect.y;
+    const currentRatio = entry.intersectionRatio;
+    const isIntersecting = entry.isIntersecting;
+
+    // Scrolling down/up
+    if (currentY < previousY) {
+      if (currentRatio > previousRatio && isIntersecting) {
+        console.log("Scrolling down enter");
+        nav.classList.add("fixed-nav");
+      } else {
+        console.log("Scrolling down leave");
+      }
+    } else if (currentY > previousY && isIntersecting) {
+      if (currentRatio < previousRatio) {
+        console.log("Scrolling up leave");
+        nav.classList.remove("fixed-nav");
+      } else {
+        console.log("Scrolling up enter");
+      }
+    }
+
+    previousY = currentY;
+    previousRatio = currentRatio;
+  });
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+observer.observe(target);
+
+scrollButton.addEventListener("click", (event) => {
+  var headerOffset =
+    nav.getBoundingClientRect().height + nav.getBoundingClientRect().bottom;
+  var elementPosition = toursSection.getBoundingClientRect().top;
+  var offsetPosition = elementPosition - headerOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth",
+  });
+  event.preventDefault();
+});
