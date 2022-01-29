a69ff77659073bd753afd668476eca1f42155fc7
giangnguyen1992 <tgn1992@hotmail.de>
2022-01-29 19:20:24 +0100

    added countdown timer

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -1,23 +1,40 @@
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+const giveawayEndDateBox = document.querySelector(".giveaway");
+const daysBox = document.querySelector(".days");
+const hourBox = document.querySelector(".hours");
+const minuteBox = document.querySelector(".minutes");
+const secondBox = document.querySelector(".seconds");
+
+const giveAwayEndDate = dayjs("2022-02-20 12:12");
+
+giveawayEndDateBox.innerText = `Giveaway Ends On ${giveAwayEndDate.format(
+  "dddd, DD MMMM YYYY, hh:mma"
+)}`;
+
+function dhm(ms) {
+  const days = Math.floor(ms / (24 * 60 * 60 * 1000));
+  const daysms = ms % (24 * 60 * 60 * 1000);
+  const hours = Math.floor(daysms / (60 * 60 * 1000));
+  const hoursms = ms % (60 * 60 * 1000);
+  const minutes = Math.floor(hoursms / (60 * 1000));
+  const minutesms = ms % (60 * 1000);
+  const sec = Math.floor(minutesms / 1000);
+  return {
+    days,
+    hours,
+    minutes,
+    sec,
+  };
+}
+
+function setDiffTime() {
+  const now = dayjs();
+
+  const different = giveAwayEndDate.diff(now);
+  daysBox.innerText = dhm(different).days;
+  hourBox.innerText = dhm(different).hours;
+  minuteBox.innerText = dhm(different).minutes;
+  secondBox.innerText = dhm(different).sec;
+}
+
+setDiffTime();
+setInterval(setDiffTime, 1000);
